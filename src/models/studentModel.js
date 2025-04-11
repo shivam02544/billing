de3eb2fea15318d7c66e5cd1c4e170e09ea4a290
Prabhat <anteshkumar114@gmail.com>
2025-04-11 18:57:56 +0530

    edited model of student

--- src/models/studentModel.js
+++ src/models/studentModel.js
@@ -1,13 +1,13 @@
 import { Schema, model, models } from "mongoose";
 const studentSchema = new Schema({
-  pageId: { type: String, set: (value) => value.toUpperCase() },
-  name: { type: String, set: (value) => value.toUpperCase() },
-  className: { type: String, set: (value) => value.toUpperCase() },
-  village: { type: String, set: (value) => value.toUpperCase() },
+  pageId: { type: String, set: (value) => value.toUpperCase().trim() },
+  name: { type: String, set: (value) => value.toUpperCase().trim() },
+  className: { type: String, set: (value) => value.toUpperCase().trim() },
+  village: { type: String, set: (value) => value.toUpperCase().trim() },
 
-  fatherName: { type: String, set: (value) => value.toUpperCase() },
+  fatherName: { type: String, set: (value) => value.toUpperCase().trim() },
 
-  contact: { type: String },
+  contact: { type: String, trim: true },
   transport: { type: Number },
   extraClassesFee: { type: Number },
   dueFee: Number,
