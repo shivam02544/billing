3ff652bd80c4d13ee9d24940482342bb80d6cf31
shivam02544 <anteshkumar114@gmail.com>
2026-05-31 16:34:01 +0530

    adding qr payment sys 2

--- src/app/getStudentsBill/page.jsx
+++ src/app/getStudentsBill/page.jsx
@@ -133,7 +133,7 @@ const Page = () => {
                                     <span>ADDRESS: {bill.village}</span>
                                 </div>
                             </div>
-                            <div className='flex flex-col w-[96%] my-1 text-xs'>
+                            <div className='flex flex-col w-[96%] my-1 text-xs relative'>
                                 {bill.tuitionFee != 0 ? <div className='flex justify-between'><span>SCHOOL FEE:</span><span>₹{bill.tuitionFee}</span></div> : <br />}
                                 {bill.transportFee != 0 ? <div className='flex justify-between'><span>TRANSPORT FEE:</span><span>₹{bill.transportFee}</span></div> : <br />}
                                 {bill.isExamFeeAdded && <div className='flex justify-between'><span>EXAM FEE:</span><span>₹{bill.examFee}</span></div>}
@@ -145,9 +145,9 @@ const Page = () => {
                                     <span>TOTAL DUES:</span>
                                     <span>₹{bill.totalDue}</span>
                                 </div>
-                            </div>
-                            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 opacity-70 pointer-events-none'>
-                                <UpiQrCode amount={bill.totalDue} billReference={bill.pageId} size={80} />
+                                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 opacity-70 pointer-events-none'>
+                                    <UpiQrCode amount={bill.totalDue} billReference={bill.pageId} size={70} />
+                                </div>
                             </div>
                             <div className='text-xs flex flex-col w-[96%] border-[1px] border-black mb-2 p-1'>
                                 <span>1. Fee Payment date is from 1st to 10th of every month.</span>
